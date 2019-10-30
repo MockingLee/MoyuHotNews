46cd702b26c2a791e8d9802d68fc0bcd3f7a4aac
MockingLee <38075220+MockingLee@users.noreply.github.com>
2019-10-30 16:06:02 +0800

    fix ignore

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <el-container>
-  <el-header>Header</el-header>
+  <el-header class="header">
+      <div class="header-left">热榜聚合</div>
+  </el-header>
   <el-main>
-    <el-container>
-      <el-row>
+    <el-container class="hot-list">
+      <el-row class="btn-row"> 
         <el-button class="select-type-btn" @click="getTypeList('zhihu')">知乎</el-button>
         <el-button class="select-type-btn" @click="getTypeList('v2ex')">v2ex</el-button>
       </el-row>
@@ -13,18 +15,18 @@
       <div class="list-container" v-loading="loading">
         <div class="list-item" v-for="(item , index) in items" :key="item">
           <el-row>
-            <div class="item-index">
-              {{++index}}
-            </div>
+            <!-- <div class="item-index">
+              
+            </div> -->
             <div class="item-title">
-              <a :href="item.url">{{item.title}}</a>
+              <a :href="item.url" class="title-container"><em class="item-index">{{++index}}</em>{{item.title}}</a>
             </div>
           </el-row>
         </div>
       </div>
     </el-container>
   </el-main>
-  <el-footer>Footer</el-footer>
+  <el-footer></el-footer>
 </el-container>
 </template>
 
@@ -62,16 +64,65 @@ export default {
 
 <style>
 .select-type-btn{
-  background-color: #DCDFE6
+  background-color: #f6f6f6
+}
+.btn-row{
+  margin: 10px 0;
 }
 .item-index{
   float: left;
   text-align: center;
-  height: 100%;
-  width: 57px;
-  
+  margin: 0 26px 0 5px;
+  display: block;
+}
+
+em{
+  font-style: italic;
+}
+.title-container{
+  display: block;
+  padding: 0 12px;
+  color: #363636;
+  font-weight: 700;
+  text-decoration: none;
+}
+.hot-list{
+  position: sticky;
+  z-index: 9;
+  top: 0;
+  border-bottom: 1px solid #ebebeb;
 }
 .item-title{
   float: left;
+  color: black;
+  
+}
+
+
+.list-container{
+  width: 100%;
+  min-height: 200px;
+}
+
+.list-item{
+  border-bottom: 1px solid #ebebeb;
+}
+
+.header{
+  background-color: #0078ff;
+  height: 60px;
+  text-align: center;
+}
+.header-left{
+  float: left;
+  height:60px;
+  line-height: 60px;
+  text-align: center;
+  margin-left: 3px;
+  color: azure;
+}
+
+.el-main{
+  padding: 0px
 }
 </style>
